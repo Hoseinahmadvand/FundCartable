ae303bb785fbf822f9cca9be63f8a4e9389cdd17
mr552002 <93993516+mr552002@users.noreply.github.com>
2023-10-21 14:50:50 +0330

    Add project files.

---
+++ FundCartable/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FundCartable.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
